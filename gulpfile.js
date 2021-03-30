c5d9807fc115b08ef32cce8cbf659ea0d38dc7db
Sergey <redmoloch@yanadex.ru>
2021-03-30 13:33:37 +0300

    remaded for project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const {src, dest, series, watch} = require('gulp');
 const sync = require('browser-sync').create();
+const del = require('del');
 
 const sourcemap = require('gulp-sourcemaps');
 const scss = require('gulp-sass');
@@ -13,30 +14,24 @@ const rename = require('gulp-rename');
 
 const fileInclude = require('gulp-file-include');
 
-// const indexHtml = () => {
-//   return src('source/html/index0.html')
-//     .pipe(fileInclude({
-//       prefix: '@@',
-//       basepath: '@file'
-//     }))
-//     .pipe(rename('index.html'))
-//     .pipe(dest('build'))
-//     .pipe(sync.stream())
-// }
+const imagemin = require('gulp-imagemin');
+const svgstore = require('gulp-svgstore');
+const svgSprite = require('gulp-svg-sprite')
+const webp = require('gulp-webp');
 
-function htmlProrto (start, end) {
-  return src(`source/html/${start}.html`)
+function htmlProto (file) {
+  return src(`source/html/${file}.html`)
     .pipe(fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(rename(`${end}.html`))
     .pipe(dest('build'))
     .pipe(sync.stream())
-
 }
 
-const indexHtml = () => htmlProrto('index0', 'index');
+const indexHtml = () => htmlProto('index');
+const formHtml = () => htmlProto('form');
+const catalogHtml = () => htmlProto('catalog');
 
 
 
@@ -47,11 +42,11 @@ const scssFiles = [
 ]
 
 const styles = () => {
-  // return src('source/scss/**/*.scss')
-  return src(scssFiles)
+  return src('source/scss/style.scss')
+  // return src(scssFiles)
     .pipe(sourcemap.init())
     .pipe(scss({ outputStyle: 'expanded' }))
-    .pipe(concat('styles.min.css'))
+    // .pipe(concat('styles.min.css'))
     // .pipe(postcss([prefix({ cascade: false })]))
     .pipe(gprefix({ cascade: false, overrideBrowserslist: ['last 5 versions'] }))
     // .pipe(csso())
@@ -60,24 +55,58 @@ const styles = () => {
     .pipe(sync.stream())
 }
 
-// const server = sync.create();
+const scriptList = ['source/js/example1.js', 'source/js/example2.js']
+const scripts = () => {
+  // return src('source/js/**/*.js')
+  return src(scriptList)
+    .pipe(concat('script.js'))
+    .pipe(dest('build/js'))
+}
 
-// const serve = (done) => {
-//   server.init({
-//     server: {
-//       baseDir: 'build'
-//     },
-//     cors: true,
-//     notify: false,
-//     ui: false,
-//   });
-//   done();
-// }
+const sprite = () => {
+  return src('source/img/**/*.svg')
+    .pipe(imagemin([imagemin.svgo({
+      plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+      ]
+    })]))
+    .pipe(svgstore())
+    .pipe(rename('sprite.svg'))
+    .pipe(dest('build/img'))
+}
 
-// const watcher = () => {
-//   watch('source/scss/**/*.scss', styles);
-//   watch('source/html/**/*.html', indexHtml);
-// }
+const images = () => {
+  return src('source/img/**/*.{svg,jpg,png}')
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 4 }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+      })
+    ]))
+    .pipe(dest('build/img'))
+}
+
+const webper = () => {
+  return src('source/img/**/*.{jpg,png}')
+    .pipe(webp())
+    .pipe(dest('build/img'))
+}
+
+const directoryList = ['source/fonts/**/*']
+
+const copy = () => {
+  return src(directoryList)
+    .pipe(dest('build'))
+}
+
+const clean = () => {
+  return del('build');
+}
 
 const watcher = () => {
   sync.init({
@@ -88,10 +117,25 @@ const watcher = () => {
 
   watch('source/scss/**/*.scss', styles);
   watch('source/html/**/*.html', indexHtml);
+  watch('source/html/**/*.html', formHtml);
+  watch('source/html/**/*.html', catalogHtml);
+  watch('source/js/**/*.js', scripts)
+  watch('source/img/**/*.svg', sprite);
+  watch('source/img/**/*.{jpg,png}', images);
+  watch('source/img/**/*.{jpg,png}', webper);
 }
 
 
 exports.styles = styles;
 exports.indexHtml = indexHtml;
+exports.formHtml = formHtml;
+exports.catalogHtml = catalogHtml;
+exports.scripts = scripts;
+exports.sprite = sprite;
+exports.images = images;
+exports.webper = webper;
+exports.copy = copy;
+exports.clean = clean;
 
-exports.default = series(indexHtml, styles, watcher)
+
+exports.default = series(clean, indexHtml, formHtml, catalogHtml, sprite, images, webper, copy, styles, scripts, watcher)
---
+++ source/js/example1.js
@@ -0,0 +1,24 @@
+// const rangeField = document.querySelector('.range__bar-wrapper');
+// const rangeBar = rangeField.querySelector('.range__bar');
+// const rangeFieldLen = rangeField.offsetWidth;
+// let rangeBarLen = rangeBar.offsetWidth;
+
+// const catsContainer = document.querySelector('.fat-to-dead__imgs');
+// const fatCat = catsContainer.querySelector('.fat-to-dead__fat-block');
+
+// console.log(catsContainer, fatCat)
+
+// rangeField.addEventListener('mousedown', (evt) => {
+//   rangeBar.style.width = evt.offsetX + 'px';
+// })
+
+// rangeField.addEventListener('mouseup', (evt) => {
+//   rangeBarLen = evt.offsetX;
+//   rangeBar.style.width = rangeBarLen + 'px';
+//   const fatCatLen = Math.floor(rangeBarLen / rangeFieldLen * catsContainer.offsetWidth) +"px";
+//   fatCat.style.width = fatCatLen
+//   console.log(rangeBarLen)
+// })
+
+
+
