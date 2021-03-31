0113a7b3dd2b4a289175a9d06109a9963a8e8cd0
Sergey <redmoloch@yanadex.ru>
2021-03-31 09:45:51 +0300

    made index and example.js

--- source/js/example1.js
+++ source/js/example1.js
@@ -1,24 +1,36 @@
-// const rangeField = document.querySelector('.range__bar-wrapper');
-// const rangeBar = rangeField.querySelector('.range__bar');
-// const rangeFieldLen = rangeField.offsetWidth;
-// let rangeBarLen = rangeBar.offsetWidth;
+const rangeField = document.querySelector('.cat-slider__range');
+const rangeBar = rangeField.querySelector('.cat-slider__bar');
+const rangeFieldLen = rangeField.offsetWidth;
+let rangeBarLen = rangeBar.offsetWidth;
 
-// const catsContainer = document.querySelector('.fat-to-dead__imgs');
-// const fatCat = catsContainer.querySelector('.fat-to-dead__fat-block');
-
-// console.log(catsContainer, fatCat)
-
-// rangeField.addEventListener('mousedown', (evt) => {
-//   rangeBar.style.width = evt.offsetX + 'px';
-// })
-
-// rangeField.addEventListener('mouseup', (evt) => {
-//   rangeBarLen = evt.offsetX;
-//   rangeBar.style.width = rangeBarLen + 'px';
-//   const fatCatLen = Math.floor(rangeBarLen / rangeFieldLen * catsContainer.offsetWidth) +"px";
-//   fatCat.style.width = fatCatLen
-//   console.log(rangeBarLen)
-// })
+const catsContainer = document.querySelector('.cat-slider__images');
+const fatCat = catsContainer.querySelector('.cat-slider__past-cat-block');
 
 
 
+rangeField.addEventListener('mousedown', (evt) => {
+  rangeBar.style.width = evt.offsetX + 'px';
+})
+
+rangeField.addEventListener('mouseup', (evt) => {
+  rangeBarLen = evt.offsetX;
+  rangeBar.style.width = rangeBarLen + 'px';
+  const fatCatLen = Math.floor(rangeBarLen / rangeFieldLen * catsContainer.offsetWidth) +"px";
+  fatCat.style.width = fatCatLen
+  console.log(rangeBarLen)
+})
+
+
+
+const btnPast = document.querySelector('.cat-slider__button--past');
+const btnNow = document.querySelector('.cat-slider__button--now');
+
+btnPast.addEventListener('click', () => {
+  rangeBar.style.width = '99%';
+  fatCat.style.width = '99%';
+})
+
+btnNow.addEventListener('click', () => {
+  rangeBar.style.width = '1%'
+  fatCat.style.width = '1%'
+})
